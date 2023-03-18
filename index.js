26e325e2de3c92241d121755d7e21fb51523182c
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2023-03-18 16:53:14 -0400

    api is ready for first test

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/hello', (req, res) => {
+    res.json('hello, there')
+})
+
+const port = process.env.PORT || 9000
+
+server.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
