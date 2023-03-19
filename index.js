57da2d670f6ce10ae91a9932614d097631dd3a91
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2023-03-18 22:38:13 -0400

    cors and helmet installed

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const server = require('./api/server')
+const cors = require("cors");
+const helmet = require("helmet");
+
 
 // const server = express()
 
 server.use(express.json())
+server.use(cors());
+server.use(helmet());
+
 
 server.get('/hello', (req, res) => {
     res.json('hello, there')
