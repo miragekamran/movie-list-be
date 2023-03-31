95c65032498dd0f561de16d633f062847a093a22
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2023-03-31 17:03:25 -0400

    wip

--- knexfile.js
+++ knexfile.js
@@ -4,23 +4,45 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/movies.db3'
+    development: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+            filename: "./data/movies.db3",
+        },
+        migrations: {
+            directory: "./data/migrations",
+        },
+        seeds: {
+            directory: "./data/seeds",
+        },
+        pool: {
+            afterCreate: (conn, done) => {
+                conn.run("PRAGMA foreign_keys = ON", done);
+            },
+        },
     },
-    migrations: {
-      directory: './data/migrations'
+    testing: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+            filename: "./data/movies.db3",
+        },
+        pool: {
+            afterCreate: (conn, done) => {
+                conn.run("PRAGMA foreign_keys = ON", done);
+            },
+        },
     },
-    seeds: {
-      directory: './data/seeds'
+    production: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: "./data/migrations",
+        },
+        seeds: {
+            directory: "./data/seeds",
+        },
     },
-    useNullAsDefault: true,
-  },
-  testing: {
-   
-  },
-  production: {
-    
-  }
 };
