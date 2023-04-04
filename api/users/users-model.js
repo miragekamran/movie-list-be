1fb807d00a6e2a86f1316bf36037276ff18e4fce
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2023-04-04 14:31:18 -0400

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,34 +1,34 @@
-// const knex = require("knex");
+const knex = require("knex");
 
-// const db = knex({
-//     client: "sqlite3",
-//     connection: {
-//         filename: "./data/movies.db3",
-//     },
-//     useNullAsDefault: true,
-// });
+const db = knex({
+    client: "sqlite3",
+    connection: {
+        filename: "./data/movies.db3",
+    },
+    useNullAsDefault: true,
+});
 
-// function find() {
-//     return db("users").select("id", "username", "email", "password");
-// }
+function find() {
+    return db("users").select("id", "username", "email", "password");
+}
 
-// function findBy(filter) {
-//     return db("users").where(filter);
-// }
+function findBy(filter) {
+    return db("users").where(filter);
+}
 
-// async function add(user) {
-//     const [id] = await db("users").insert(user, "id").returning("id");
+async function add(user) {
+    const [id] = await db("users").insert(user, "id").returning("id");
 
-//     return findById(id);
-// }
+    return findById(id);
+}
 
-// function findById(id) {
-//     return db("users").where({ id }).first();
-// }
+function findById(id) {
+    return db("users").where({ id }).first();
+}
 
-// module.exports = {
-//     add,
-//     find,
-//     findBy,
-//     findById,
-// };
+module.exports = {
+    add,
+    find,
+    findBy,
+    findById,
+};
